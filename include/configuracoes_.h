f73efde14c0ea3c09fb419783f21b0373fd6dc52
Maycon <pm.maycon@gmail.com>
2021-01-26 14:47:42 -0300

    Limpeza no arquivo de configuracão

--- include/configuracoes_.h
+++ include/configuracoes_.h
@@ -1,5 +1,3 @@
-
-
 //Estação
 #define INTERVALOENTRELEITURAS 3 //em minutos
 #define SERVIDORNTP "br.pool.ntp.org"
@@ -10,18 +8,8 @@
 
 //Comunicação servidor HTTP
 #define ENDERECOSERVIDOR "Servidor" //endereço do servidor (String ou IP)
-#define TENTATIVASSERVIDORHTTP 1    //número máximo de tentativas de conexão com o servidor HTTP
-#define TENTATIVASDHCP 2            //número maximo de tentativas de conexão via DHCP
-#define SEGUNDOSANTESDEABORTAR 10   //quantidade de segundos antes de abortar a conexão com o servidor HTTP (durante uma requisição)
-
-//Armazenamento
-#define DIRETORIODADOS "/"                 //deve começar e terminar com barras
-#define EXTENSAOARQUIVOS ".txt"            // deve começar com ponto
-#define EXTENSAOARQUIVOSTEMPORARIOS ".tmp" // deve começar com ponto
 
 //Pluviômetro
 #define VOLUMEBASCULA 0.2
 #define TEMPOMINIMOENTREBASCULADAS 2000
 #define PINOPLUVIOMETRO 18
-
-#define RESETDIARIO
